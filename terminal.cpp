f96bc5e31a50d7937178a54529ec6a5942c5fae7
SagarikaShrote <42342995+SagarikaShrote@users.noreply.github.com>
2019-08-24 01:02:57 +0530

    final file
    this is the final file made the previous one was incomplete

--- terminal.cpp
+++ terminal.cpp
@@ -13,18 +13,6 @@
 #include<iostream>
 using namespace std;
 
-//signal handlers ctr+c/z
-void sigintHandler(int sig_num)
-{
-	
-	signal(SIGINT, sigintHandler); 
-}
-
-void sighandler(int sig_num) 
-{
-	
-    signal(SIGTSTP, sighandler); 
-} 
 
 //printing username and current working directory
 void printDir() 
@@ -34,6 +22,15 @@ void printDir()
 	char* username = getenv("USER");
 	printf("\n%s @ %s $",username, cwd); 
 } 
+int checkredir(string command){
+	if(command.find(">")!=-1){
+		return -1;
+	}
+	else if(command.find("<")!=-1){
+		return -2;
+	}
+	else return 0;
+}
 
 int check(string command){
 	int c,res=0;
@@ -47,31 +44,15 @@ int check(string command){
 	else{
 		res=2;
 	}
+	if (res==0){
+		res=checkredir(command);
+	}
 	return res;
 
 }
-
-int main(){
-
-	signal(SIGTSTP, sighandler); 
-	signal(SIGINT, sigintHandler); 
-	
-	cout<<"\t WELCOME";
-
-	vector<string> result;
-	string command;
-	do{
-		
-		result.clear();
-		printDir();
-		getline(cin,command);
-		int type=check(command);
-		
-		if(command=="exit" || command==" " || command==""){
-			//do nothing
-		}
-		else if(type==0){
-			boost::split(result,command,boost::is_any_of(" ")); 
+//to handle standalone commands
+void singlec(string command, vector<string> & result){
+	boost::split(result,command,boost::is_any_of(" ")); 
 			if(result[0]=="cd"){					//for cd only
 				if(result.size()==1){
 					cout<<"->";
@@ -100,18 +81,18 @@ int main(){
 			  	}
 			  	carr[n]=NULL;
 			    if (pid == -1) { 
-			        printf("\nFailed forking child.."); 
+			        printf("Process Creation failed\n");
 			        
 			    } else if (pid == 0) { 
-			    	//signal(SIGTSTP,  SIG_DFL); 
-					//signal(SIGINT,  SIG_DFL); 
+			    	signal(SIGTSTP,SIG_IGN); 
+					signal(SIGINT,SIG_DFL); 
 			        if (execvp(carr[0], carr) < 0) { 
-			            printf("\nCould not execute command.."); 
+			            printf("\nShell: Incorrect command."); 
 			        } 
 			        exit(0); 
 			    } else { 
 			        // waiting for child to terminate 
-			        wait(NULL);  
+			        //wait(NULL);  
 			        int i=0;
 			        for(i=0;i<n;i++){
 			        	free(carr[i]);
@@ -119,28 +100,146 @@ int main(){
 			        //free(carr);
 			    } 
 			}
+}
+//it is assumed that at a time only one redirection is allowed and in which one side there will be command and other side will be file. It does not work on two files.
+//input redirection 
+void execipredir(string command,vector<string>& result){
+	boost::trim_if(command, boost::is_any_of("<"));
+	boost::split(result,command,boost::is_any_of("<"),boost::token_compress_on);
+	int rc = fork();
+	if(rc==0){
+		signal(SIGTSTP,SIG_IGN); 
+		signal(SIGINT,SIG_DFL); 
+		int n = result[1].size();
+		char carr[n+1];
+		strcpy(carr, result[1].c_str());
+		//file like f0<f1
+		int f1; 	
+		//for input redirection f1 must be a file so it must open in read only permissions as it must not be changed
+		if((f1 = open(carr, O_RDONLY) )< 0){
+			perror("can't open file:");
+			exit(0);
 		}
+		dup2(f1, 0);
+		close(f1);
+		vector<string> v;
+		singlec(result[0],v);
+		wait(NULL);
+		exit(0);
+	}
+	else if(rc<0){
+		printf("Process Creation failed\n");
+	}
+}
+//output redirection
+void execopredir(string command,vector<string>& result){
+	boost::trim_if(command, boost::is_any_of(">"));
+	boost::split(result,command,boost::is_any_of(">"),boost::token_compress_on);
+	int rc = fork();
+	if(rc==0){
+		signal(SIGTSTP,SIG_IGN); 
+		signal(SIGINT,SIG_DFL); 
+		int n = result[1].size();
+		char carr[n+1];
+		strcpy(carr, result[1].c_str());
+		//file like f0>f1
+		int f1; 	
+		//for output redirection also f1 must be a file so it must open in write or append permissions as it must be changed
+
+		if((f1 = open(carr, O_CREAT|O_WRONLY|O_APPEND, S_IRWXU)) < 0){
+			perror("can't open file:");   
+			exit(0);                 
+		}
+		dup2(f1,1);
+		close(f1);
+		vector<string> v;
+		singlec(result[0],v);
+		wait(NULL);
+		exit(0);
+	}
+	else if(rc<0){
+		printf("Process Creation failed\n");
+	}
+}
+
+int main(){
+
+	signal(SIGTSTP, SIG_IGN); 
+	signal(SIGINT, SIG_IGN); 
+	
+	cout<<"\t WELCOME";
+
+	vector<string> result;
+	string command;
+	do{
+		
+		result.clear();
+		printDir();
+		getline(cin,command);
+		int type=check(command);
+		
+		if(command=="exit" || command==" " || command==""){
+			//do nothing
+		}
+		else if(type==0){
+			//call single command fn
+			singlec(command,result);
+			wait(NULL); 
+		}
+		//will not give space seperated commands for && and &&& this is the assumption
 		else if(type==1){
 			boost::trim_if(command, boost::is_any_of("&&"));
 			boost::split(result,command,boost::is_any_of("&&"),boost::token_compress_on); 
-			if(result.size()==1){
-				
-			}
-			else{
-			
+			//cout<<"checkpt1 &&"<<endl;
+			vector<string>temp;
+			for(int i=0;i<result.size();i++){
+				temp.clear();
+				type=check(result[i]);
+				switch(type){
+					case 0:	singlec(result[i],temp);
+						break;
+					//case 2: it is assumed that all tasks can be in either parallel or serial there will not be miscleneous tasks
+					case -1:execopredir(result[i],temp);
+						break;
+					case -2:execipredir(result[i],temp);
+						break;
+				}
+				wait(NULL);
 			}
 
 		}
-		else{
+		else if(type==2){
 			boost::trim_if(command, boost::is_any_of("&&&"));
 			boost::split(result,command,boost::is_any_of("&&&"),boost::token_compress_on); 
-			if(result.size()==1){
+			//cout<<"checkpt2 &&&"<<endl;
+			vector<string>temp;
+			for(int i=0;i<result.size();i++){
+				temp.clear();
+				type=check(result[i]);
+				switch(type){
+					case 0:	singlec(result[i],temp);
+						break;
+					//case 1: it is assumed that all tasks can be in either parallel or serial there will not be miscleneous tasks
+					case -1:execopredir(result[i],temp);
+						break;
+					case -2:execipredir(result[i],temp);
+						break;
+				}
 				
 			}
-			else{
-				
-				
+			for(int i=0;i<result.size();i++){
+				wait(NULL);
 			}
+			
+		}
+		//it is assumed that functionality like pipe etc is not used
+		else if(type==-1){
+			execopredir(command,result);
+			wait(NULL);
+		}
+		else if(type==-2){
+			execipredir(command,result);
+			wait(NULL);
 		}
 		
 	}while(command!="exit");
